fc192033f1bab940c2468560cbab7d32e8a11611
ManJav <mansurjavadi@gmail.com>
2026-09-14 00:04:06 +0300

    fixbugs - leaderbord serialization

--- package/Runtime/Helpers/LeaderboardHelper.cs
+++ package/Runtime/Helpers/LeaderboardHelper.cs
@@ -8,57 +8,63 @@ namespace TinyNakama.Helpers
     {
         public LeaderboardHelper(NakamaClient client) : base(client) { }
 
-        public async Task<NakamaLeaderboardList> Records(string leaderboardId, int limit = 50, string cursor = null)
+        public async Task<LeaderboardList> Records(string leaderboardId, int limit = 50, string cursor = null)
         {
             var path = $"/v2/leaderboard/{Uri.EscapeDataString(leaderboardId)}?limit={limit}";
             if (!string.IsNullOrEmpty(cursor)) path += $"&cursor={Uri.EscapeDataString(cursor)}";
             var response = await client.SendAsync(UnityWebRequest.kHttpVerbGET, path);
-            
-            var records = NakamaClient.Deserialize<NakamaLeaderboardList>(response, "leaderboard list");
-            foreach (var record in records.records)
+            var result = NakamaClient.Deserialize<LeaderboardList>(response, "leaderboard list", true);
+            foreach (var record in result.records)
             {
-                record.metaData = NakamaClient.Deserialize<LeaderboardMetadata>(record.metadata, "leaderboard metadata");
+                record.info = NakamaClient.Deserialize<LeaderboardRecordInfo>(record.metadata, "leaderboard info");
             }
-            return records;
+            return result;
         }
 
-        public async Task<NakamaLeaderboardList> GetAroundOwner(string leaderboardId, string ownerId, int limit = 3)
+        public async Task<LeaderboardList> GetAroundOwner(string leaderboardId, string ownerId, int limit = 3)
         {
-            var path = $"/v2/leaderboard/{Uri.EscapeDataString(leaderboardId)}/around/{Uri.EscapeDataString(ownerId)}?limit={limit}";
+            var path = $"/v2/leaderboard/{Uri.EscapeDataString(leaderboardId)}/owner/{Uri.EscapeDataString(ownerId)}?limit={limit}";
             var response = await client.SendAsync(UnityWebRequest.kHttpVerbGET, path);
-            return NakamaClient.Deserialize<NakamaLeaderboardList>(response, "leaderboard around owner");
+            var result = NakamaClient.Deserialize<LeaderboardList>(response, "leaderboard around owner", true);
+            foreach (var record in result.records)
+            {
+                record.info = NakamaClient.Deserialize<LeaderboardRecordInfo>(record.metadata, "leaderboard info");
+            }
+            return result;
         }
     }
 
 
     [Serializable]
-    public sealed class NakamaLeaderboardRecord
+    public sealed class LeaderboardList
     {
-        public string leaderboardId;
-        public string ownerId;
-        public string username;
-        public long score;
-        public long subscore;
-        public int rank;
-        public string metadata;
-        public string createTime;
-        public string updateTime;
-        public string expiryTime;
-        public LeaderboardMetadata metaData;
-    }
-
-    [Serializable]
-    public sealed class NakamaLeaderboardList
-    {
-        public NakamaLeaderboardRecord[] records;
+        public LeaderboardRecord[] records;
         public string nextCursor;
         public string prevCursor;
     }
 
     [Serializable]
-    public sealed class LeaderboardMetadata
+    public sealed class LeaderboardRecord
+    {
+        public string leaderboardId;
+        public string ownerId;
+        public string username;
+        public long score;
+        public int rank;
+        public string metadata;
+        public string createTime;
+        public string updateTime;
+        public string expiryTime;
+        public int numScore, maxNumScore;
+        public LeaderboardRecordInfo info;
+    }
+
+    [Serializable]
+    public sealed class LeaderboardRecordInfo
     {
         public int avatarUrl;
-        public string displayName,location;
+        public string displayName;
+        public string location;
     }
+
 }
